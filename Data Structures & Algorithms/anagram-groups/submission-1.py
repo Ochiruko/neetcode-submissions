12ea44df7cc1c7a44b63df167a9be83ea3073768
Dylan Sommer <dylansommer96@gmail.com>
2026-05-16 18:03:31 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:    
+    # I use a clever workaround to having frozendicts: sorting.
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams: dict[str, list[str]] = dict()
+        for s in strs:
+            s_chars = "".join(sorted(s))
+            if s_chars not in anagrams:
+                anagrams[s_chars] = []
+            anagrams[s_chars].append(s)
+        return list(anagrams.values())
